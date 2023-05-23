c7b49ae87125e1f0e5ef8b5f8d2376a18ab8fde5
tiphaine pinson <tiphaine.pinson@hotmail.fr>
2023-05-23 21:40:43 +0200

    re ecriture appel fetch

--- Portfolio-architecte-sophie-bluel/FrontEnd/Script/works.js
+++ Portfolio-architecte-sophie-bluel/FrontEnd/Script/works.js
@@ -1,12 +1,71 @@
-// appel du fetch
 
-const reponse = await fetch("http://localhost:5678/api/works");
-const projets = await reponse.json();
 
-if(reponse.ok){
-    console.log(projets);
-}else{
-    throw new Error("Impossible d'acceder aux travaux !");
-}
+// const gallery = document.querySelector(".gallery");
 
-const portfolio = document.querySelector("#portfolio");
+// async function getWorks() {
+//     const response = await fetch('http://localhost:5678/api/works'); 
+//     const works = await response.json();
+//     return works
+//   }
+
+ 
+//   getWorks().then(works => {
+//   console.log(works)
+//     for( item of works) {
+//       gallery.innerHTML += `      
+//       <figure>
+// 					<img src="${item.imageUrl}" alt="${item.title}">
+// 					<figcaption>${item.title}</figcaption>
+// 				</figure>
+//       `;
+//     }
+
+// })
+// .catch (erreur => {
+// console.log(erreur.message)
+// }) ;
+
+
+// displayWorks =(Works) => {
+
+
+
+// }
+
+        //APPEL DU FETCH
+
+async function getWorks() {
+          const response = await fetch('http://localhost:5678/api/works'); 
+          const works = await response.json();
+           
+          if(reponse.ok){
+          console.log(works);
+      }else{
+          throw new Error("erreur lors de l'appel API.");
+      }
+        }
+
+        // BALISE DE RATTACHEMENT
+
+const portfolio = document.querySelector(".gallery");
+
+        // RECUPERATION
+
+        function Projets(works){
+          projets.forEach(element => {
+              
+              const carteProjet = document.createElement("figure");
+              const imageProjet = document.createElement("img");
+              imageProjet.src = element.imageUrl;
+              const titreProjet = document.createElement("figcaption");
+              titreProjet.innerHTML = element.title;
+              
+              portfolio.appendChild(sectionGallery);
+              sectionGallery.appendChild(carteProjet);
+              carteProjet.appendChild(imageProjet);
+              carteProjet.appendChild(titreProjet);
+              
+          });
+          
+      }
+            Projets(works);
