29b5dfd90dc5ce0f34052cfc72b15c390307ee11
tiphaine pinson <tiphaine.pinson@hotmail.fr>
2023-06-28 21:15:27 +0200

    cacher les éléments

--- Portfolio-architecte-sophie-bluel/FrontEnd/Script/connect.js
+++ Portfolio-architecte-sophie-bluel/FrontEnd/Script/connect.js
@@ -3,10 +3,12 @@ const isAdmin = localStorage.getItem("token") ? true : false;
 
 const login = document.getElementById('login');
 const logout = document.getElementById('logout');
+
 const navEdition = document.getElementById('navEdition')
-const filtres = document.getElementsByName('filtres')
-const modify1 = document.getElementsByName('modify1')
-const modify2 = document.getElementsByName('modify2')
+const filtres = document.getElementById('filtres')
+
+const modify1 = document.querySelector('.modify1')
+const modify2 = document.querySelector('.modify2')
 
 logout.addEventListener('click' , function() {    
     localStorage.clear();
@@ -17,7 +19,9 @@ if(isAdmin) {
 
     login.style.display = 'none';
     logout.style.display = 'inline';
+
     filtres.style.display = 'none';
+
     modify1.style.display = 'inline';
     modify2.style.display = 'inline';
 
@@ -28,8 +32,11 @@ else
 {
     login.style.display = 'inline';
     logout.style.display = 'none';
-    navEdition.style.display = 'none';
+
     filtres.style.display = 'inline';
+
+    navEdition.style.display = 'none';
+    
     modify1.style.display = 'none';
     modify2.style.display = 'none';
 
