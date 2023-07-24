a32079efeb29923e4dafc861219a9a0a5e710721
tiphaine pinson <tiphaine.pinson@hotmail.fr>
2023-07-24 15:24:41 +0200

    Supprimer une image de la gallerie

--- Portfolio-architecte-sophie-bluel/FrontEnd/Script/modal.js
+++ Portfolio-architecte-sophie-bluel/FrontEnd/Script/modal.js
@@ -39,70 +39,74 @@ closeBtn.addEventListener("click", closeModal);
 /*Création de la constante closeModal */
 function closeModal() {
 
-    
-    
+   
+   
      Modal1.style.display = 'none' /* cache la modale */
      modalAjout.style.display = "none";
     modalPhoto.style.display='block';
-    
-    
+   
+   
 };
 
 // fonction pour la fermeture de la modale quand on clique en dehors
 
 
 function closeModalClickOut(event) {
-    
-    if (event.target === modal) {
-        modal.style.display = 'none';
+   
+    if (event.target === Modal1) {
+        Modal1.style.display = 'none';
       }
     }
 
-    document.addEventListener("click", closeModalClickOut);
-  
+    Modal1.addEventListener("click", closeModalClickOut);
+ 
 
 
 //récupère les projets depuis l'API et les ajoute à la modal
 function getProjectModal() {
-    console.log(worksList)
-    worksList.forEach(function (project) { 
-        addProjectToModal(project); 
+    const modalGallery = document.querySelector(".modalGallery");
+    modalGallery.innerHTML="";
+   
+    worksList.forEach(function (project) {
+        addProjectToModal(project);
     })
 }
 
-// Ajouter les photos à la modal 
+// Ajouter les photos à la modal
 function addProjectToModal(project) {
 
     const modalGallery = document.querySelector(".modalGallery");
 
 
-    const figure = document.createElement("figure"); 
+    const figure = document.createElement("figure");
     figure.classList.add("figureModal");
 
-    const img = document.createElement("img"); 
+    const img = document.createElement("img");
     img.classList.add("imgModal");
-    img.src = project.imageUrl; 
+    img.src = project.imageUrl;
     img.width = 100;
 
-    const figcaption = document.createElement("figcaption"); 
+    const figcaption = document.createElement("figcaption");
     figcaption.classList.add("figCaption");
-    figcaption.alt = project.title; 
-    figcaption.textContent = "éditer"; 
+    figcaption.alt = project.title;
+    figcaption.textContent = "éditer";
 
-    const categoryId = document.createElement("p"); 
-    categoryId.src = project.categoryId; 
+    const categoryId = document.createElement("p");
+    categoryId.src = project.categoryId;
 
-    const deleteWork = document.createElement("i"); 
-    deleteWork.classList.add("deleteTrashIcon", "fa", "fa-solid", "fa-trash-can"); 
-    deleteWork.dataset.id = project.id;
+    const deleteWork = document.createElement("i");
+    deleteWork.classList.add("deleteTrashIcon", "fa", "fa-solid", "fa-trash-can");
+    // deleteWork.dataset.id = project.id;
+
+    deleteWork.addEventListener("click", function() {deletePicture(project.id)})
 
     let arrows = document.createElement("i");
     arrows.classList.add("fa-solid", "fa-arrows-up-down-left-right");
-    
-    figure.append(img, figcaption, categoryId, deleteWork,arrows); 
+   
+    figure.append(img, figcaption, categoryId, deleteWork,arrows);
 
     modalGallery.append(figure);
-    
+   
 }
 
 // Ouvrir Ajouter une photo avec l'évènement click
@@ -111,12 +115,12 @@ const ajout = document.getElementById("ajout-image");
 ajout.addEventListener("click", openPicture);
 
 function openPicture() {  
-    
+   
     modalAjout.style.display = "block";
     modalPhoto.style.display='none';
   }
 
-//   Clique pour le retour en arrière 
+//   Clique pour le retour en arrière
 
 const back = document.getElementById("arrowBack");
 
@@ -129,39 +133,35 @@ function arrow(){
 
 //Partie pour supprimer une image de la galerie (au click de la poubelle)
 
-async function deletePicture(e) {
-  if (e.target.classList.contains('deleteTrashIcon')) {
-      const projectId = e.target.dataset.id;
-      console.log(sessionStorage.getItem('token'));
-      const response = await fetch("http://localhost:5678/api/works/" + projectId, {
+async function deletePicture(id) {
+ 
+      const projectId = id;  
+
+    if (confirm("Voulez-vous supprimer l'image?") == true) {
+        fetch("http://localhost:5678/api/works/" + projectId, {
           method: 'DELETE',
           headers: {
-              "Content-Type": "application/json; charset=utf-8",
-              "authorization": `Bearer ${sessionStorage.getItem('token')}`
-          }
+            'accept' : '*/*',
+            'Authorization' : 'Bearer ' + (localStorage.getItem("token")),
+         }
+      }).then (() => {
+        getWorks();
       });
-      if (response.ok) {
-          if (confirm("Voulez-vous supprimer l'image?") == true) {
-              e.target.parentElement.remove(); //suppression du target
-          } else {
-              modalAjout;
-          }
-      } else {
-          console.log("Une erreur s'est produite lors de la suppression du projet.");
-      }
-  }
-};
+       
+ 
+}
+}
 
 function validateImage() {
     //Création des photos
     let ajoutPhotoBouton = document.getElementById("ajoutPhotoBtn");
     let ajoutPhotoLabel = document.getElementById("ajoutPhotoLabel");
     let imgContainer = document.getElementById("imgContainer");
-    
+   
         //appel de la fonction pour vérifier si le fichier est sous un format valide
         //Condition Si il n'y a pas de fichier
-        if (ajoutPhotoBouton.files.length == 0) { 
-            return; //La fonction s'arrête la 
+        if (ajoutPhotoBouton.files.length == 0) {
+            return; //La fonction s'arrête la
         }
         //sinon
         else {
@@ -176,17 +176,17 @@ function validateImage() {
                 else {
                     const imgFile = document.createElement('img');
                     let imgErrorMessage = document.createElement("span");
-    
+   
                     imgFile.setAttribute("id", "imgPreview");
                     imgFile.setAttribute('alt', 'Aperçu de l\'image sélectionnée');
-                    
+                   
                     imgErrorMessage.classList.add("imgErrorMessage"); // création des messages d'erreurs
 
                     imgContainer.appendChild(imgFile, imgErrorMessage); // ajout de l'élément imgFile au parent imgContainer
-    
+   
                     imgFile.src = URL.createObjectURL(ajoutPhotoBouton.files[0]); // création de l'url de la photo ajoutée
                     imgFile.className = 'img-uploaded';
-                    
+                   
                     // Invisibilité des autres éléments de l'image container quand on preview l'image uploadée
                     ajoutPhotoLabel.style.display = "none";
                     let ajoutPhotoIcon = document.getElementById("ajoutPhotoIcon");
@@ -214,7 +214,7 @@ const fileTypes = [
     "image/jpg"
 ];
 
-// Fonction pour gerer les erreurs pour le TITRE 
+// Fonction pour gerer les erreurs pour le TITRE
 function validateTitle() {
     let inputTitle = document.getElementById("titrePhoto");
     let errors = false;
@@ -252,24 +252,24 @@ function validateFile() {
 
 // Ajout d'un élément
 async function validateFormProject() {
-    
+   
 
     // Récupération des saisies pour la création du nouvel élément
     const imgUploaded = document.getElementById("ajoutPhotoBtn").files[0];
-    
+   
     const inputTitle = document.getElementById("titrePhoto").value;
-    
+   
     const selectCategorie = document.getElementById("categoriePhoto");
     const categoriePhotoId = selectCategorie.options[selectCategorie.selectedIndex].dataset.id;
-    
-  
+   
+ 
 
     // Construction du formData à envoyer
     const formData = new FormData();
     formData.append("image", imgUploaded);
     formData.append("title", inputTitle);
     formData.append("category", categoriePhotoId);
-  
+ 
     // Appel de la fonction fetch avec toutes les informations nécessaires
     let response = await fetch("http://localhost:5678/api/works", {
       method: "POST",
@@ -283,14 +283,14 @@ async function validateFormProject() {
     // Condition si on réussi à rajouter l'image
     if (response.status === 200 || 201) {
         alert('L\'ajout de l\'image a été réalisé avec succès');
-        
-    
+       
+   
     } else if (response.status === 401 || 400) {
         alert('Veuillez ajouter un titre ou image');
         console.log("Action impossible");
     };
-    
-} 
+   
+}
 ;
 
 //Changement de couleur du bouton validé
